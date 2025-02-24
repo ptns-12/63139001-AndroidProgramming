1d5a6bb36215007c4d18098c2210bbeea001fb8b
ptns-12 <104245476+ptns-12@users.noreply.github.com>
2025-02-24 21:07:39 +0700

    Xử lý backend

--- AppCong/app/src/main/java/ptns/ntu/appcong/MainActivity.java
+++ AppCong/app/src/main/java/ptns/ntu/appcong/MainActivity.java
@@ -1,6 +1,8 @@
 package ptns.ntu.appcong;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +22,27 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+
+    //bộ lắng nghe và xử lý sự kiện click lên nút Tính tổng
+    public void XuLyCong(View view){
+        //Tìm, tham chiếu đến điều khiển trên tệp XML, mapping sang java file
+        EditText editTextSoA = findViewById(R.id.edtA);
+        EditText editTextSoB = findViewById(R.id.edtB);
+        EditText editTextKetQua = findViewById(R.id.edtKQ);
+        // Lấy dữ liệu về ở điều khiển số a
+        String strA = editTextSoA.getText().toString();   //strA = "2"
+        // Lấy dữ liệu về ở điều khiển số b
+        String strB = editTextSoB.getText().toString();   //strB = "4"
+
+        // Chuyển dữ liệu sang dạng số
+        int so_A = Integer.parseInt(strA);  //2
+        int so_B = Integer.parseInt(strB);  //4
+        //Tính toán theo yêu cầu
+        int tong = so_A + so_B;             //6
+        String strTong = String.valueOf(tong); // chuyển sang dạng chuỗi; "6"
+        //Hiện ra màn hình
+        editTextKetQua.setText(strTong);
+
     }
 }
