d50f2dfdc1d53015c8410e1904097eecdc55684e
ptns-12 <104245476+ptns-12@users.noreply.github.com>
2025-03-05 23:53:16 +0700

    C3: Activity is listener

---
+++ Ex5_2/app/src/main/java/ptns/ntu/ex5_2/MainActivity.java
@@ -0,0 +1,52 @@
+package ptns.ntu.ex5_2;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    EditText editTextSo1, editTextSo2, editTextKQ;
+    Button nutCong, nutTru, nutNhan, nutChia;
+
+    void TimDieuKhien() {
+        editTextSo1 = findViewById(R.id.edtSo1);
+        editTextSo2 = findViewById(R.id.edtSo2);
+        editTextKQ = findViewById(R.id.edtKetQua);
+        nutCong = findViewById(R.id.btnCong);
+        nutTru = findViewById(R.id.btnTru);
+        nutNhan = findViewById(R.id.btnNhan);
+        nutChia = findViewById(R.id.btnChia);
+
+        // Set sự kiện click cho các button
+        nutCong.setOnClickListener(this);
+        nutTru.setOnClickListener(this);
+        nutNhan.setOnClickListener(this);
+        nutChia.setOnClickListener(this);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        TimDieuKhien();
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+
+    @Override
+    public void onClick(View v) {
+        
+
+    }
+}
