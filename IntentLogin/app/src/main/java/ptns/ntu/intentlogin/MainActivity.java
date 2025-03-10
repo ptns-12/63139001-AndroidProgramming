119df92fd15b8642909e20a00be6ff4dc8b86813
ptns-12 <104245476+ptns-12@users.noreply.github.com>
2025-03-10 18:23:49 +0700

    xulynutbam

--- IntentLogin/app/src/main/java/ptns/ntu/intentlogin/MainActivity.java
+++ IntentLogin/app/src/main/java/ptns/ntu/intentlogin/MainActivity.java
@@ -1,6 +1,9 @@
 package ptns.ntu.intentlogin;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,6 +18,19 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        //tim dkhien
+        Button btnDangNhap = (Button) findViewById(R.id.btnLogin);
+        //
+        btnDangNhap.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //xu ly
+                Intent iLogin = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(iLogin);
+            }
+        });
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
