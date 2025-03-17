8504ea69f203ac118d5dc6740cfc961b06132334
ptns-12 <104245476+ptns-12@users.noreply.github.com>
2025-03-17 23:15:08 +0700

    update listview

--- ListView/app/src/main/java/ptns/ntu/listview/MainActivity.java
+++ ListView/app/src/main/java/ptns/ntu/listview/MainActivity.java
@@ -1,7 +1,11 @@
 package ptns.ntu.listview;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -16,7 +20,6 @@ public class MainActivity extends AppCompatActivity {
     //khai bao du lieu
     ArrayList<String> dsNgonNguLT;
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,6 +32,29 @@ public class MainActivity extends AppCompatActivity {
         });
 
         listViewNNLT = findViewById(R.id.lvNNLT);
+        //b1. chuan bi du lieu
+        dsNgonNguLT = new ArrayList<String>();
+        dsNgonNguLT.add("python");
+        dsNgonNguLT.add("php");
+        dsNgonNguLT.add("java");
+        //b2
+        ArrayAdapter<String> adapterNNLT;
+        adapterNNLT = new ArrayAdapter<>(
+                this, android.R.layout.simple_list_item_1,dsNgonNguLT
+        );
+        //b3 gan adapter
+        listViewNNLT.setAdapter(adapterNNLT);
+
+        //b4 gan bo lang nghe va xu ly su kien
+        listViewNNLT.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                //xu ly
+                String giaTriDuocChon = dsNgonNguLT.get(position);
+                //
+                Toast.makeText(MainActivity.this, giaTriDuocChon, Toast.LENGTH_SHORT).show();
+            }
+        });
 
     }
 }
