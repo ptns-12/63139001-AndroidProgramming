f57e22444ae44f800334f1e296ae3e2fbd58991e
ptns-12 <104245476+ptns-12@users.noreply.github.com>
2025-04-04 14:02:50 +0700

    update chucnang2

--- thigk2.phamtrannhatsang/app/src/main/java/ptns/ntu/thigk2phamtrannhatsang/ChucNang2Activity.java
+++ thigk2.phamtrannhatsang/app/src/main/java/ptns/ntu/thigk2phamtrannhatsang/ChucNang2Activity.java
@@ -1,9 +1,7 @@
 package ptns.ntu.thigk2phamtrannhatsang;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,25 +9,20 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class ChucNang2Activity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        Button btnchucnang2 = findViewById(R.id.btn1);
-        btnchucnang2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intencn2 = new Intent(MainActivity.this,ChucNang2Activity.class);
-                startActivity(intencn2);
-            }
-        });
+        setContentView(R.layout.activity_chuc_nang2);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+    public void TinhDiemTB(View view){
+
     }
 }
